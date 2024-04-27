f0a74926175159916e3e157cb4ef85cf7f14a4b8
Dmitriy <mavashicorp@gmail.com>
2024-04-27 13:07:51 +0300

    111derfgdfg

---
+++ FinMiniApi/FinMiniApi/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+//app.UseHttpsRedirection();
+var hotels = new List<Hotel>();
+
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) => {
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0)
+    {
+        throw new Exception("Not found");
+    }
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) => {
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) throw new Exception("Not found");
+    hotels.RemoveAt(index);
+});
+
+
+app.Run();
+
+
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
